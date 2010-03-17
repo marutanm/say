b7a386029ef5e55b06b08da9766528062bcdfee8
marutanm <marutanm@Mbear.local>
2010-03-17 23:40:39 +0900

    say.rb: almost complete dictionary.txt: a little changed

--- say.rb
+++ say.rb
@@ -2,6 +2,7 @@
 require 'dictionary'
 
 def concat(array)
+    p "say " + array.join(' ')
     return "say " + array.join(' ')
 end
 
@@ -12,20 +13,26 @@ end
 def splitText(text)
     # p text
     i = 0
-    p = 0
+    point = 0
     splited = Array.new()
     while i < text.length
         if text.slice(i,1) =~ /[aiueo]/
-            print i,text.slice(i,1)
+            # print i , " : " , text.slice(i,1),"\n"
+            # p text.slice(point..i)
+            splited.push(text.slice(point..i))
+            point = i + 1
         end
         i += 1
     end
+    return splited
 end
 
-p ARGV, ARGV.class
-splitText(ARGV[0])
-
-
+p ARGV#, ARGV.class
+splited = splitText(ARGV[0])
+d = KanaDict.new
+words = d.phonetic(splited)
+p words
+say(words)
 
 
 # say(ARGV)
