0c38cf15bc1f80be2849d50407f99b4aaa0104e0
marutanm <marutanm@gmail.com>
2010-03-19 19:38:27 +0900

    enable to handling multiple argument

--- say.rb
+++ say.rb
@@ -10,30 +10,31 @@ def say(words)
     system(concat(words))
 end
 
-def splitText(text)
-    # p text
+def splitText(array)
     i = 0
     point = 0
     splited = Array.new()
-    while i < text.length
-        if text.slice(i,1) =~ /[aiueo]/
-            # print i , " : " , text.slice(i,1),"\n"
-            # p text.slice(point..i)
-            splited.push(text.slice(point..i))
-            point = i + 1
+    array.map{|text|
+        p text
+        while i < text.length
+            if text.slice(i,1) =~ /[aiueo]/
+                # print i , " : " , text.slice(i,1),"\n"
+                # p text.slice(point..i)
+                splited.push(text.slice(point..i))
+                point = i + 1
+            end
+            i += 1
         end
-        i += 1
-    end
+        i = 0
+        point = 0
+    }
     return splited
 end
 
 p ARGV#, ARGV.class
-splited = splitText(ARGV[0])
 d = KanaDict.new
-words = d.phonetic(splited)
+words = d.phonetic(splitText(ARGV))
 p words
 say(words)
 
-
-# say(ARGV)
 exit
